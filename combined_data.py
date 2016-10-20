80113a5cd473b6d00a3219ece852ad29c178a3e5
Evgeny Kurbatsky <kurbatsky@gmail.com>
2016-10-20 22:31:11 +0200

    Add check for different dataset consistency

--- combined_data.py
+++ combined_data.py
@@ -23,7 +23,7 @@ def shared_dataset(data_xy):
     return shared_x, shared_y
 
 
-files_list = ["esophagus",
+dataset_names = ["esophagus",
               "gastric",
               "H1_cell_line",
               "H1_derived_mesenchymal_stem_cells",
@@ -42,7 +42,7 @@ def divide_data(index):
     if not os.path.exists(divided_path):
         x_list = []
         y_list = []
-        for name in files_list:
+        for name in dataset_names:
             x, y = prepare_xy(name)
             x_list.append(x)
             y_list.append(y)
@@ -93,11 +93,17 @@ def prepare_xy(name):
     return x, y
 
 
-def prepare_data(data):
+def prepare_data(data, sclice=None):
     train, valid, test = data
 
     def prepossess(d):
-        return shared_dataset(zip(*d))
+        x, y = zip(*d)
+        if sclice is not None:
+            x = [t[sclice:sclice + 1] for t in x]
+            y = [t[sclice:sclice + 1] for t in y]
+            return shared_dataset((x, y))
+        else:
+            return shared_dataset((x, y))
 
     return prepossess(train), prepossess(valid), prepossess(test)
 
@@ -107,7 +113,8 @@ class Fitter(object):
                  training,
                  validation,
                  test,
-                 batch_size):
+                 batch_size,
+                 data_set_size):
         self.batch_size = batch_size
         train_set_x, train_set_y = training
         validation_set_x, validation_set_y = validation
@@ -121,7 +128,9 @@ class Fitter(object):
 
         index = T.lscalar()  # index to a [mini]batch
 
-        output_layer = self.create_chip_seq_network(batch_size, x)
+        input_size = 14
+
+        output_layer = self.create_chip_seq_network(batch_size, x, data_set_size, input_size)
 
         output = lasagne.layers.get_output(output_layer)
 
@@ -162,9 +171,7 @@ class Fitter(object):
                     y: test_set_y[index * self.batch_size:(index + 1) * self.batch_size]}
         )
 
-    def create_chip_seq_network(self, batch_size, x):
-        input_size = 14
-        data_set_size = len(files_list)
+    def create_chip_seq_network(self, batch_size, x, data_set_size, input_size):
         l_in = lasagne.layers.InputLayer(shape=(batch_size, data_set_size, input_size), input_var=x)
 
         l_reshape = lasagne.layers.ReshapeLayer(l_in, (batch_size, data_set_size * input_size))
@@ -198,14 +205,14 @@ def get_test_error(network):
     return test_err
 
 
-def get_error_from_seq(data, logger):
+def get_error_from_seq(data, logger, data_set_size):
     train, validation, test = data
     train_x, train_y = train
     batch_size = 1000
 
     train_batches_number = train_x.get_value().shape[0] // batch_size
 
-    fitter = Fitter(train, validation, test, batch_size)
+    fitter = Fitter(train, validation, test, batch_size, data_set_size)
     best_error = 1000
     patience = 100
     result_error = 0
@@ -248,12 +255,40 @@ def main():
 
     epoch_start = time.time()
 
+    data_set_num = len(dataset_names)
+
+    errors = np.zeros((data_set_num, 5))
+
+    for s in range(data_set_num):
+        for i in range(5):
+            data = prepare_data(divide_data(i + 1), s)
+            logger.log("start: {} {}".format(dataset_names[s], i))
+            fitter_logger = FileLogger(result_directory, "log_{}_{}".format(dataset_names[s], i))
+
+            error = get_error_from_seq(data, fitter_logger, 1)
+            fitter_logger.close()
+            logger.log("error: {}".format(error))
+            errors[s, i] = error
+
+    for s in range(data_set_num):
+        print dataset_names[s]
+        print errors[s]
+
+    errors = np.zeros((data_set_num, 5))
+
     for i in range(5):
         data = prepare_data(divide_data(i + 1))
         fitter_logger = FileLogger(result_directory, "log_{}".format(i))
-        error = get_error_from_seq(data, fitter_logger)
+
+        error = get_error_from_seq(data, fitter_logger, data_set_num)
+        errors[:, i] = error
         fitter_logger.close()
         logger.log("error: {}".format(error))
+        logger.log("mean: {}".format(error.mean()))
+
+    for s in range(data_set_num):
+        print dataset_names[s]
+        print errors[s]
 
     epoch_end = time.time()
     logger.log("time:{}".format(human_time(epoch_end - epoch_start)))
